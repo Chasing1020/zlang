af3610da6a8532c119d78e59771e60bb43702ee9
Chasing1020 <chasing1020@gmail.com>
2022-03-15 20:35:03 +0800

    fix bugs

--- main.js
+++
@@ -1,7 +0,0 @@
-let factory = function (i) {
-    if (i == 0) {
-        return 1;
-    } else {
-        return i * factory(i - 1);
-    }
-}
--- runtime/builtin.go
+++ runtime/builtin.go
@@ -155,23 +155,4 @@ func init() {
 		}
 		return newError("invalid literal for min(), only accepts integers.")
 	}
-
-	builtinFunctions["append"] = func(args ...object.Object) object.Object {
-		if len(args) != 2 {
-			return newError("wrong number of arguments. got=%d, want=2",
-				len(args))
-		}
-		if args[0].Type() != object.ARRAY {
-			return newError("argument to `push` must be ARRAY, got %s",
-				args[0].Type())
-		}
-		arr := args[0].(*object.Array)
-		length := len(arr.Elements)
-
-		newElements := make([]object.Object, length+1, length+1)
-		copy(newElements, arr.Elements)
-		newElements[length] = args[1]
-
-		return &object.Array{Elements: newElements}
-	}
 }
